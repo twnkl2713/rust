789155c3368650a10b3d8446da9c9c94023ac80e
twinkle485 <twinklepalbrb@yahoo.com>
2025-05-27 21:38:12 +0530

    string

---
+++ src/bin/7_string.rs
@@ -0,0 +1,40 @@
+#![allow(unused)]
+
+fn main(){
+    // String -> mutate or data needs to be owned
+    let msg = String::from("Hello Rust👋");
+    let len = msg.len();
+    println!("msg: {msg}");
+    println!("len: {len}");
+    // &str -> read only (immutable)
+    let msg = String::from("Hello rust");
+    let s = &msg[0..5];
+    let len = s.len();
+    println!("slice: {s}");
+    println!("len: {len}");
+    
+    let hello = "Hello Rust";
+    
+    // Multi line string literal
+    let s = r#"
+        { "a": 1,
+          "b": { "c": 2 },
+          "d": 3
+        }
+    "#;
+    println!("{s}");
+
+    // Deref coercian
+    let msg = String::from("Hello Twinkle");
+    let s = &msg[0..5];
+
+    // Add &str to msg
+    let mut msg = "Yo".to_string();
+    msg += "!";
+    println!("{msg}");
+
+    let lang = "Rust";
+    let emoji = "🦀";
+    let msg = format!("Hello {lang} {emoji}");
+    println!("{}", msg);
+}
