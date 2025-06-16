3fb6f95131ec54f76760d7e7425014879ae4a0a2
twinkle485 <twinklepalbrb@yahoo.com>
2025-06-16 11:51:57 +0530

    updated code

---
+++ src/bin/51_move.rs
@@ -0,0 +1,26 @@
+#![allow(unused)]
+
+fn f(s: &mut String) {}
+
+fn main() {
+    let mut s = "rust".to_string();
+    f(&mut s);
+    println!("{s}");
+
+    // closures can capture variables by 
+    // -borrow immutable reference &T
+    // -borrow mutable reference &mut T
+    // -take ownership of value T
+
+    // Borrow immutable reference &T
+    let s = "rust".to_string();
+    let f = || println!("borrow: {s}");
+    f();
+    println!("main: {s}");
+
+    // Borrow mutable reference &mut T
+    let mut s = "rust".to_string();
+    let mut f = || s += " world";
+    f();
+    println!("main: {s}");
+}
