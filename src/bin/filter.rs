2e4fa46ef8d54387c4aaeaf3a1704df01150cfa6
twinkle485 <twinklepalbrb@yahoo.com>
2025-07-04 11:37:19 +0530

    new code

--- src/bin/filter.rs
+++ src/bin/filter.rs
@@ -1,9 +1,18 @@
 fn main() {
-    let v1: Vec<i32> = vec![1, 2, 3];
+    let v1: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8];
     let iter = v1.iter();
+    let new_iter = v1.iter();
     let iter2 = iter.filter(|x| *x % 2 == 0); // filters out even
+    let iter3 = new_iter.filter(|x| *x % 2 == 1).map(|x| x * 2); // filters out odd, then returns their double
+    
+    println!("iter2: ");
     for x in iter2 {
-        println!("{}", x);
+        print!("{} ", x);
     }
+    println!("\niter3: ");
+    for i in iter3 {
+        print!("{} ", i);
+    }
+    println!("\nv1: ");
     println!("{:?}", v1);
 }
---
+++ src/bin/filter1ass.rs
@@ -0,0 +1,11 @@
+fn filter_and_map(v: Vec<i32>) -> Vec<i32> {
+    let new_iter = v1.iter().filter(|x| *x % 2 == 1).map(|x| x + 1);
+    let new_vec: Vec<i32> = new_iter.collect();
+    return new_vec;
+}
+
+fn main() {
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let ans = filter_and_map(v1);
+    println!("{:?}", ans);
+}
